ac51fbbf1519e9a4ac9849cfbee5debef6ca5734
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-03-16 23:18:02 +0300

    feat: solve the solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,38 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const cutStrToArr = (word, number) => {
+    const result = [];
+    for (let i = 0; i < word.length; i += 1) {
+        if (i % number === 0) {
+            result.push(word.substr(i, number));
+        }
+    }
+    return result;
 }
+
+function decode(expr) {
+   
+    let str = '';
+    let decodeStr = '';
+    
+    for (let i=0; i < expr.length; i+=10) {
+        let subExp = expr.slice(i, i+10);
+
+        if (subExp === '**********') decodeStr += ' ';
+        else {
+            for (let j=0; j<subExp.length; j+=2) {
+                if (subExp[j] + '' + subExp[j+1] === '10') str+='.';
+                else if (subExp[j] + '' + subExp[j+1] === '11') str+='-';
+            }
+
+            decodeStr += MORSE_TABLE[str];
+            str = '';
+        }
+    }
+  
+    return decodeStr;
+  }
 
 module.exports = {
     decode
